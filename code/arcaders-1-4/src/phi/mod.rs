2a5c047dda8d2d64a4822c2585f3be20ffcf3b21
Jessy Pelletier-Lemire <jessyplemire@gmail.com>
2015-10-18 13:12:42 -0400

    Updated arcaders 1.4

---
+++ code/arcaders-1-4/src/phi/mod.rs
@@ -0,0 +1,48 @@
+#[macro_use]
+mod events;
+
+use ::sdl2::render::Renderer;
+
+
+struct_events! {
+    keyboard: {
+        key_escape: Escape,
+        key_up: Up,
+        key_down: Down
+    },
+    else: {
+        quit: Quit { .. }
+    }
+}
+
+
+/// Bundles the Phi abstractions in a single structure which
+/// can be passed easily between functions.
+pub struct Phi<'window> {
+    pub events: Events,
+    pub renderer: Renderer<'window>,
+}
+
+
+/// A `ViewAction` is a way for the currently executed view to
+/// communicate with the game loop. It specifies which action
+/// should be executed before the next rendering.
+pub enum ViewAction {
+    None,
+    Quit,
+}
+
+
+pub trait View {
+    /// Called when `self` becomes the main, rendered view.
+    fn resume(&mut self, context: &mut Phi);
+
+    /// Called when `self` stops being the main, rendered view.
+    fn pause(&mut self, context: &mut Phi);
+
+    /// Called on every frame to take care of both the logic and
+    /// the rendering of the current view.
+    ///
+    /// `elapsed` is expressed in seconds.
+    fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction;
+}
