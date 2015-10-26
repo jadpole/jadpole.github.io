30fa900cb839e4fbea7d123b7b7b68367365aa48
Jessy Pelletier-Lemire <jessyplemire@gmail.com>
2015-10-26 09:05:44 -0400

    Added ArcadeRS 1.7

---
+++ code/arcaders-1-7/src/phi/mod.rs
@@ -0,0 +1,169 @@
+#[macro_use]
+mod events;
+pub mod data;
+pub mod gfx;
+
+use ::sdl2::render::Renderer;
+
+
+struct_events! {
+    keyboard: {
+        key_escape: Escape,
+        key_up: Up,
+        key_down: Down,
+        key_left: Left,
+        key_right: Right,
+        key_space: Space
+    },
+    else: {
+        quit: Quit { .. }
+    }
+}
+
+
+/// Bundles the Phi abstractions in a single structure which
+/// can be passed easily between functions.
+pub struct Phi<'window> {
+    pub events: Events,
+    pub renderer: Renderer<'window>,
+}
+
+impl<'window> Phi<'window> {
+    fn new(events: Events, renderer: Renderer<'window>) -> Phi<'window> {
+        ::sdl2_image::init(::sdl2_image::INIT_PNG);
+
+        Phi {
+            events: events,
+            renderer: renderer,
+        }
+    }
+
+    pub fn output_size(&self) -> (u32, u32) {
+        self.renderer.output_size().unwrap()
+    }
+}
+
+impl<'window> Drop for Phi<'window> {
+    fn drop(&mut self) {
+        ::sdl2_image::quit();
+    }
+}
+
+
+/// A `ViewAction` is a way for the currently executed view to
+/// communicate with the game loop. It specifies which action
+/// should be executed before the next rendering.
+pub enum ViewAction {
+    None,
+    Quit,
+    ChangeView(Box<View>),
+}
+
+
+pub trait View {
+    /// Called on every frame to take care of both the logic and
+    /// the rendering of the current view.
+    ///
+    /// `elapsed` is expressed in seconds.
+    fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction;
+}
+
+
+/// Create a window with name `title`, initialize the underlying libraries and
+/// start the game with the `View` returned by `init()`.
+///
+/// # Examples
+///
+/// Here, we simply show a window with color #ffff00 and exit when escape is
+/// pressed or when the window is closed.
+///
+/// ```
+/// struct MyView;
+///
+/// impl View for MyView {
+///     fn resume(&mut self, _: &mut Phi) {}
+///     fn pause(&mut self, _: &mut Phi) {}
+///     fn render(&mut self, context: &mut Phi, _: f64) -> ViewAction {
+///         if context.events.now.quit {
+///             return ViewAction::Quit;
+///         }
+///
+///         context.renderer.set_draw_color(Color::RGB(255, 255, 0));
+///         context.renderer.clear();
+///         ViewAction::None
+///     }
+/// }
+///
+/// spawn("Example", |_| {
+///     Box::new(MyView)
+/// });
+/// ```
+pub fn spawn<F>(title: &str, init: F)
+where F: Fn(&mut Phi) -> Box<View> {
+    // Initialize SDL2
+    let sdl_context = ::sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+    let mut timer = sdl_context.timer().unwrap();
+
+    // Create the window
+    let window = video.window(title, 800, 600)
+        .position_centered().opengl().resizable()
+        .build().unwrap();
+
+    // Create the context
+    let mut context = Phi::new(
+        Events::new(sdl_context.event_pump().unwrap()),
+        window.renderer()
+            .accelerated()
+            .build().unwrap());
+
+    // Create the default view
+    let mut current_view = init(&mut context);
+
+
+    // Frame timing
+    let interval = 1_000 / 60;
+    let mut before = timer.ticks();
+    let mut last_second = timer.ticks();
+    let mut fps = 0u16;
+
+    loop {
+        // Frame timing (bis)
+
+        let now = timer.ticks();
+        let dt = now - before;
+        let elapsed = dt as f64 / 1_000.0;
+
+        // If the time elapsed since the last frame is too small, wait out the
+        // difference and try again.
+        if dt < interval {
+            timer.delay(interval - dt);
+            continue;
+        }
+
+        before = now;
+        fps += 1;
+
+        if now - last_second > 1_000 {
+            println!("FPS: {}", fps);
+            last_second = now;
+            fps = 0;
+        }
+
+
+        // Logic & rendering
+
+        context.events.pump(&mut context.renderer);
+
+        match current_view.render(&mut context, elapsed) {
+            ViewAction::None =>
+                context.renderer.present(),
+
+            ViewAction::Quit =>
+                break,
+
+            ViewAction::ChangeView(new_view) =>
+                current_view = new_view,
+        }
+    }
+}
---
+++ code/arcaders-1-7/src/views/mod.rs
@@ -0,0 +1,151 @@
+use ::phi::{Phi, View, ViewAction};
+use ::phi::data::Rectangle;
+use ::phi::gfx::{CopySprite, Sprite};
+use ::sdl2::pixels::Color;
+
+
+/// Pixels traveled by the player's ship every second, when it is moving.
+const PLAYER_SPEED: f64 = 180.0;
+
+const SHIP_W: f64 = 43.0;
+const SHIP_H: f64 = 39.0;
+
+
+/// The different states our ship might be in. In the image, they're ordered
+/// from left to right, then from top to bottom.
+#[derive(Clone, Copy)]
+enum ShipFrame {
+    UpNorm   = 0,
+    UpFast   = 1,
+    UpSlow   = 2,
+    MidNorm  = 3,
+    MidFast  = 4,
+    MidSlow  = 5,
+    DownNorm = 6,
+    DownFast = 7,
+    DownSlow = 8
+}
+
+
+struct Ship {
+    rect: Rectangle,
+    sprites: Vec<Sprite>,
+    current: ShipFrame,
+}
+
+
+pub struct ShipView {
+    player: Ship,
+}
+
+impl ShipView {
+    pub fn new(phi: &mut Phi) -> ShipView {
+        let spritesheet = Sprite::load(&mut phi.renderer, "assets/spaceship.png").unwrap();
+        let mut sprites = Vec::with_capacity(9);
+
+        for y in 0..3 {
+            for x in 0..3 {
+                sprites.push(spritesheet.region(Rectangle {
+                    w: SHIP_W,
+                    h: SHIP_H,
+                    x: SHIP_W * x as f64,
+                    y: SHIP_H * y as f64,
+                }).unwrap());
+            }
+        }
+
+        ShipView {
+            player: Ship {
+                rect: Rectangle {
+                    x: 64.0,
+                    y: 64.0,
+                    w: SHIP_W,
+                    h: SHIP_H,
+                },
+                sprites: sprites,
+                current: ShipFrame::MidNorm,
+            }
+        }
+    }
+}
+
+impl View for ShipView {
+    fn render(&mut self, phi: &mut Phi, elapsed: f64) -> ViewAction {
+        if phi.events.now.quit || phi.events.now.key_escape == Some(true) {
+            return ViewAction::Quit;
+        }
+
+
+        // Move the player's ship
+
+        let diagonal =
+            (phi.events.key_up ^ phi.events.key_down) &&
+            (phi.events.key_left ^ phi.events.key_right);
+
+        let moved =
+            if diagonal { 1.0 / 2.0f64.sqrt() }
+            else { 1.0 } * PLAYER_SPEED * elapsed;
+
+        let dx = match (phi.events.key_left, phi.events.key_right) {
+            (true, true) | (false, false) => 0.0,
+            (true, false) => -moved,
+            (false, true) => moved,
+        };
+
+        let dy = match (phi.events.key_up, phi.events.key_down) {
+            (true, true) | (false, false) => 0.0,
+            (true, false) => -moved,
+            (false, true) => moved,
+        };
+
+        self.player.rect.x += dx;
+        self.player.rect.y += dy;
+
+        // The movable region spans the entire height of the window and 70% of its
+        // width. This way, the player cannot get to the far right of the screen, where
+        // we will spawn the asteroids, and get immediately eliminated.
+        //
+        // We restrain the width because most screens are wider than they are high.
+        let movable_region = Rectangle {
+            x: 0.0,
+            y: 0.0,
+            w: phi.output_size().0 as f64 * 0.70,
+            h: phi.output_size().1 as f64,
+        };
+
+        // If the player cannot fit in the screen, then there is a problem and
+        // the game should be promptly aborted.
+        self.player.rect = self.player.rect.move_inside(movable_region).unwrap();
+
+
+        // Select the appropriate sprite of the ship to show.
+        self.player.current =
+            if dx == 0.0 && dy < 0.0       { ShipFrame::UpNorm }
+            else if dx > 0.0 && dy < 0.0   { ShipFrame::UpFast }
+            else if dx < 0.0 && dy < 0.0   { ShipFrame::UpSlow }
+            else if dx == 0.0 && dy == 0.0 { ShipFrame::MidNorm }
+            else if dx > 0.0 && dy == 0.0  { ShipFrame::MidFast }
+            else if dx < 0.0 && dy == 0.0  { ShipFrame::MidSlow }
+            else if dx == 0.0 && dy > 0.0  { ShipFrame::DownNorm }
+            else if dx > 0.0 && dy > 0.0   { ShipFrame::DownFast }
+            else if dx < 0.0 && dy > 0.0   { ShipFrame::DownSlow }
+            else { unreachable!() };
+
+
+        // Clear the scene
+        phi.renderer.set_draw_color(Color::RGB(0, 0, 0));
+        phi.renderer.clear();
+
+        // Render the bounding box (for debugging purposes)
+        phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
+        phi.renderer.fill_rect(self.player.rect.to_sdl().unwrap());
+
+        // Render the ship
+        phi.renderer.copy_sprite(
+            &self.player.sprites[self.player.current as usize],
+            self.player.rect);
+
+
+        ViewAction::None
+    }
+}
